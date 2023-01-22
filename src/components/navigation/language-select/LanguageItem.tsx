16d82b0a6d46c447167faa27f7997c972025de42
Thomas Bakker <thomasbakker@hotmail.co.uk>
2023-01-22 18:18:38 +0000

    invalid import in Language selector caught by build failure...

--- src/components/navigation/language-select/LanguageItem.tsx
+++ src/components/navigation/language-select/LanguageItem.tsx
@@ -1,9 +1,6 @@
 import { ListItemIcon, ListItemText, MenuItem } from "@mui/material";
 import styles from "./LanguageItem.module.css";
 import React from "react";
-//This needs removing - probably have a folder of language icons
-import { US } from "country-flag-icons/react/3x2";
-import { LanguageType } from "../../../types/locale";
 
 export const LanguageItem = ({ data, onChange }: any) => {
 	console.log({ data });
