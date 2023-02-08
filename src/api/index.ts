77cf7abc77a0a39a080ab2de134975f9eb20d1eb
Thomas Bakker <thomasbakker@hotmail.co.uk>
2023-02-08 14:16:49 +0000

    Amend a couple of typescript errors
    - Again over zealous use of any to remove warning

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import { BING_NEWS_SEARCH_API_OBJECT } from "./bing/news/constants";
 
 //api configs
-export const API_LIST = {
+export const API_LIST: any = {
 	bingNewsSearch: BING_NEWS_SEARCH_API_OBJECT,
 };
--- src/factories/components.ts
+++ src/factories/components.ts
@@ -9,7 +9,7 @@ import { SimpleList } from "../components/data/list/SimpleList";
 //Perhaps needs to be a much better way
 //this could theoretically be millions if you can get user components
 //data led somehow
-export const COMPONENTS = {
+export const COMPONENTS: any = {
 	["Test"]: Test, //can we return a dynamic load i.e. () => dynamic load ('@/components/containers/stack/DTAStack')
 	["SimpleList"]: SimpleList,
 	["ArticleStub"]: ArticleStub,
