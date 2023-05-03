ca5f607e099796b4bd44971372714891f41e46e0
Thomas Bakker <thomasbakker@hotmail.co.uk>
2023-05-03 13:34:16 +0100

    Added crude back button and functionality to edit nav

--- src/components/header/edit/EditNavigationMenu.tsx
+++ src/components/header/edit/EditNavigationMenu.tsx
@@ -1,7 +1,9 @@
-import { AppBar, Box, Container, Stack, Toolbar } from "@mui/material";
-import React, { ReactElement } from "react";
+import { AppBar, Box, Container, Stack, Toolbar, Button } from "@mui/material";
+import React, { ReactElement, useContext } from "react";
 import classes from "./EditNavigationMenu.module.scss";
 import { EditNavigation } from "./EditNavigation";
+import { EditContext } from "@/src/context/edit-context";
+import { useUser } from "@/src/hooks/useUser";
 
 export type EditNavigationMenuProps = {
 	prefix?: () => ReactElement | ReactElement[];
@@ -9,6 +11,37 @@ export type EditNavigationMenuProps = {
 	styles?: {
 		readonly [key: string]: string;
 	};
+};
+
+const BackButton = () => {
+	const { user } = useUser();
+	const username = user?.username || "";
+	const editCtx = useContext(EditContext);
+
+	const currentPage = editCtx.currentPage;
+
+	//editCtx.setCurrentPageHandler(`/users/${username}${route}`);
+	const onClickHandler = () => {
+		if (currentPage === `/users/${username}`) {
+			return;
+		}
+		let pages = currentPage?.split("/");
+		pages?.pop();
+		// pages?.join('/');
+		const backPage = pages?.join("/") || "";
+
+		editCtx.setCurrentPageHandler(backPage);
+	};
+
+	return (
+		<Button
+			sx={{ color: "black" }}
+			onClick={() => onClickHandler()}
+			key={"NavigationEditBackButton"}
+		>
+			Back
+		</Button>
+	);
 };
 
 //This is not really up to standard - not going to work as the main header in mobile - i.e. desktop only
@@ -23,7 +56,12 @@ export const EditNavigationMenu = ({
 			<Container>
 				<nav>
 					<Toolbar className={styles.toolbar}>
-						<Stack direction="row">{prefix()}</Stack>
+						<Stack direction="row">
+							<>
+								<BackButton />
+								{prefix()}
+							</>
+						</Stack>
 						{/* name or something */}
 						<Box sx={{ overflow: "hidden" }}>
 							<EditNavigation />
