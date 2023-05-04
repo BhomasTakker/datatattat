e3a9a677520730ca5fe6f7a7ff3b638185965d99
Thomas Bakker <thomasbakker@hotmail.co.uk>
2023-05-04 17:26:12 +0100

    Removed refetches by default from react-query
    - Needed to upgrade Azure account :/

--- src/hoc/query/withQuery.tsx
+++ src/hoc/query/withQuery.tsx
@@ -30,6 +30,12 @@ const WithQuery = ({ componentObject, queryObject }: WithQueryParams) => {
 	//update query data of id data when pagination changes?
 	const { data } = useQuery([queryId, queryState], () => queryFn(queryState), {
 		...options,
+
+		// force refetch to false until we have chaching properly in place
+		refetchInterval: false,
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+		refetchOnReconnect: false,
 	});
 
 	// console.log("Redraw Query");
