62ea04c069d1ded3cbcecce2e010971c273f36c6
Thomas Bakker <thomasbakker@hotmail.co.uk>
2023-06-05 19:21:24 +0100

    fix: ComponentEdit info
    left in test value

--- config/styles/styles.config.ts
+++ config/styles/styles.config.ts
@@ -1,5 +1,7 @@
 // TEXT.SIZES.SMALL ??
 
+//We want to use different values when phone etc
+
 export enum MARGINS {
 	VSMALL = "0.25rem",
 	SMALL = "0.5rem",
@@ -9,6 +11,7 @@ export enum MARGINS {
 	LARGE = "2.5rem",
 }
 
+//On mobile STANDARD_LEFT, etc needs to be 0
 export enum INFO_MARGINS {
 	STANDARD_LEFT = "11rem",
 	STANDARD_RIGHT = "3rem",
--- src/components/forms/edit/ComponentEdit.tsx
+++ src/components/forms/edit/ComponentEdit.tsx
@@ -85,7 +85,6 @@ export const ComponentEdit = ({
 					<Box paddingLeft={MARGINS.LARGE} width={"100%"}>
 						<WithInfo
 							infoId="selectComponent"
-							info="WordPress taxonomies (categories, tags, and other custom taxonomies) make it easy for your visitors to find the content they are looking for on your website, and there are many ways in which they can do that. You could, for instance, add a most popular tag cloud to your website, allow your visitors to search by category, display recent posts from a category, and more. In this tutorial, we will be showing you how to set up RSS"
 							marginLeft={INFO_MARGINS.STANDARD_LEFT}
 							marginRight={INFO_MARGINS.ARRAY_CONTROLS_RIGHT}
 						>
