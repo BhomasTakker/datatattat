dcd9dbe27597b1bd36b9bb0804a0e414c4314918
Thomas Bakker <thomasbakker@hotmail.co.uk>
2023-06-16 20:59:47 +0100

    minor: Just exampling nesting regions with BBC

--- src/rss/news/BBC/config.ts
+++ src/rss/news/BBC/config.ts
@@ -14,7 +14,7 @@ const POSTFIX = "/rss.xml";
 enum ENDPOINTS {
 	"Top Stories" = "news",
 	World = "news/world",
-	Uk = "news/uk",
+	UK = "news/uk",
 	Africa = "news/africa",
 	Asia = "news/asia",
 	China = "news/asia/china",
@@ -40,6 +40,59 @@ enum ENDPOINTS {
 	Leicester = "news/england/leicester",
 }
 
+enum UK_ENDPOINTS {
+	"Top Stories" = "",
+	England = "england",
+	Scotland = "scotland",
+	Wales = "wales",
+	"N. Ireland" = "northern_ireland",
+	Alba = "naidheachdan",
+	Cymru = "cymrufyw",
+	"Isle of Man" = "isle_of_man",
+	Guernsey = "guernsey",
+	Jersey = "jersey",
+}
+
+enum ENGLAND_ENDPOINTS {
+	"Top Stories" = "",
+	"North East" = "",
+	"North West" = "",
+	"Yorkshire & Lincolnshire" = "",
+	"East Midlands" = "",
+	"West Midlands" = "",
+	"West & South West" = "",
+	"East" = "",
+	"South" = "",
+	"London & South East" = "",
+	"Isle of Man & Channel Islands" = "",
+}
+
+const englandDirectoryObject = {
+	id: "bbc_england_directory",
+	label: "Region",
+	baseUrl: `${BASE_URL}/news/england`,
+	postfix: POSTFIX,
+	type: "select",
+	endpoints: ENGLAND_ENDPOINTS,
+	defaultEndpoint: "Top Stories",
+	params: [],
+};
+
+const ukDirectoryObject = {
+	id: "bbc_uk_directory",
+	label: "UK News",
+	baseUrl: `${BASE_URL}/news/uk`,
+	postfix: POSTFIX,
+	type: "select",
+	endpoints: UK_ENDPOINTS,
+	defaultEndpoint: "Top Stories",
+	params: [],
+
+	endpointObjects: {
+		England: englandDirectoryObject,
+	},
+};
+
 const inputObject = {
 	id: "bbc_endpoint",
 	label: "BBC Select Endpoint",
@@ -49,6 +102,9 @@ const inputObject = {
 	endpoints: ENDPOINTS,
 	defaultEndpoint: "Top Stories",
 	params: [],
+	endpointObjects: {
+		UK: ukDirectoryObject,
+	},
 };
 
 export const BBC_RSS_CONFIG_OBJECT = {
