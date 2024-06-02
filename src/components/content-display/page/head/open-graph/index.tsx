46c3fc34ac6d865c5292355bdbd8294d4b4e2615
Thomas Bakker <thomasbakker@hotmail.co.uk>
2024-06-02 14:05:47 +0100

    fix: add property to metatags for og

--- src/components/content-display/page/head/open-graph/index.tsx
+++ src/components/content-display/page/head/open-graph/index.tsx
@@ -13,36 +13,42 @@ const DEFAULT = [
 		key={"og:description"}
 		data-rh="true"
 		name="og:description"
+		property="og:description"
 		content="Palestinian officials say more than 300 bodies have been found at Nasser and al-Shifa hospitals."
 	></meta>,
 	<meta
 		key={"og:image"}
 		data-rh="true"
 		name="og:image"
+		property="og:image"
 		content="https://ichef.bbci.co.uk/news/1024/branded_news/10D07/production/_133217886_mediaitem133217885.jpg"
 	></meta>,
 	<meta
 		key={"og:image:alt"}
 		data-rh="true"
 		name="og:image:alt"
+		property="og:image:alt"
 		content="Palestinian civil defence workers dig mounds of earth in the grounds of Nasser hospital in Khan Younis, in the southern Gaza Strip (21 April 2024)"
 	></meta>,
 	<meta
 		key={"og:locale"}
 		data-rh="true"
 		name="og:locale"
+		property="og:locale"
 		content="en_GB"
 	></meta>,
 	<meta
 		key={"og:site_name"}
 		data-rh="true"
 		name="og:site_name"
+		property="og:site_name"
 		content="BBC News"
 	></meta>,
 	<meta
 		key={"og:title"}
 		data-rh="true"
 		name="og:title"
+		property="og:title"
 		content="UN rights chief 'horrified' by mass grave reports at Gaza hospitals"
 	></meta>,
 	<meta key={"og:type"} data-rh="true" name="og:type" content="article"></meta>,
@@ -50,6 +56,7 @@ const DEFAULT = [
 		key={"og:url"}
 		data-rh="true"
 		name="og:url"
+		property="og:url"
 		content="https://www.bbc.com/news/world-middle-east-68881325"
 	></meta>,
 ];
@@ -61,7 +68,13 @@ export const renderOpenGraphMeta = ({ data }: OpenGraph) => {
 	for (const key in data) {
 		const value = data[key] as string;
 		metadata.push(
-			<meta key={key} data-rh="true" name={key} content={value}></meta>
+			<meta
+				key={key}
+				data-rh="true"
+				name={key}
+				property={key}
+				content={value}
+			></meta>
 		);
 	}
 
