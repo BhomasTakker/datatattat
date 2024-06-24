8c5dde69b51cdca0559d996fe351b82656cc6c6f
Thomas Bakker <thomasbakker@hotmail.co.uk>
2024-06-24 12:53:11 +0100

    Remove redundancies

--- src/components/content-display/new-article/collection/grid/Grid.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-import styles from "./Grid.module.scss";
-
-type ArticleGrid = {};
-
-export const ArticleGrid = ({ children }: PropsWithChildren<ArticleGrid>) => {
-	return <div className={styles.root}>{children}</div>;
-};
