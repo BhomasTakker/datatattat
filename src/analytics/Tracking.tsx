82844384428789fef06eb5701fd9fe5bc9e0b9fa
Thomas Bakker <thomasbakker@hotmail.co.uk>
2024-08-08 20:10:21 +0100

    change speed insights import

--- src/analytics/Tracking.tsx
+++ src/analytics/Tracking.tsx
@@ -1,4 +1,7 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
+// For next version < 13.3
+// versions > 13.3 import { SpeedInsights } from '@vercel/speed-insights/next';
+// https://vercel.com/docs/speed-insights/quickstart#add-the-speedinsights-component-to-your-app
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react";
 
 export const Tracking = () => {
